a7076ba545d3dc4fef5c7cde41577ad149724180
alexo <alexo@herodigital.com>
2024-07-02 22:09:04 +0300

    Redeploy

--- tools/picker/src/index.js
+++ tools/picker/src/index.js
@@ -7,8 +7,8 @@ import getProductsInCategory from './queries/products.graphql.js';
 
 import './styles.css';
 
-const configFile = ' https://main--aem-boilerplate-commerce--hlxsites.hlx.live/configs.json';
-const defaultConfig = 'prod';
+const configFile = 'https://main--subzero--herodigital.hlx.page/configs.json';
+const defaultConfig = 'stage';
 
 /**
  * List of blocks to be available in the picker.
