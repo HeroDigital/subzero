1ee1efee0942aa0823db2f0575e91ab6a95d78e0
Nick Loiko <nick@devbook.local>
2024-07-04 20:19:28 +0200

    demo styles

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -130,7 +130,7 @@ export default async function decorate(block) {
     });
   }
 
-  const navTools = nav.querySelector('.nav-tools');
+  const navTools = nav.querySelector('.nav-sections');
 
   // Minicart
   const minicartButton = document.createRange().createContextualFragment(`<div class="minicart-wrapper">
